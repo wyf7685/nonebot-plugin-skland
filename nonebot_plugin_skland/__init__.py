cfe34182c6dcb142272e6787da27bf62de5e774a
FrostN0v0 <1614591760@qq.com>
2026-07-25 17:07:59 +0800

    :bookmark: 0.7.0

--- nonebot_plugin_skland/__init__.py
+++ nonebot_plugin_skland/__init__.py
@@ -41,7 +41,7 @@ __plugin_meta__ = PluginMetadata(
     supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
     extra={
         "author": "FrostN0v0 <1614591760@qq.com>",
-        "version": "0.6.11",
+        "version": "0.7.0",
     },
 )
 __plugin_meta__.extra.update(extra_data)
